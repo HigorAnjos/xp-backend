5f09e33fdffd6fbcecb51cc322051f1a5e15305c
Higor Anjos <higorc.anjos@gmail.com>
2022-07-17 16:28:17 -0300

    feat: :recycle: ajustando o banco para test e consertando nome da model

--- database/seeders/20220716033902-Clients.js
+++ database/seeders/20220716033902-Clients.js
@@ -4,6 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Clients',
     [
       {
+        id: 1,
         email: 'george@gmail.com',
         // usamos a função CURRENT_TIMESTAMP do SQL para salvar a data e hora atual nos campos `createdAt` e `updatedAt`
         password: '$2b$10$ajyRS5vR6nXitR.DDAmNhOmanpq0027XBSJwjdi/i6R2IvTfGQpGm', //  password:1234
@@ -11,20 +12,30 @@ module.exports = {
         updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       {
+        id: 2,
         email: 'isaac@gmail.com',
         password: '$2b$10$AaUxzPF1MCwtshu/h9VJKuJS94bA8dK5KstKlHH/l4XxSgWAirxwm', //  password:1234
         createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
         updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       {
+        id: 3,
         email: 'frank@gmail.com',
         password: '$2b$10$E4xfY.M9HkwYmsTOBaEEjulDlsJZa97sOZNyJOJvBLlb3ZyEGSpxq', //  password:1234
         createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
         updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       {
+        id: 4,
         email: 'Julio@gmail.com',
         password: '$2b$10$m/D3cWHmEZ9zHMefRw3BHu6zZHft65Vo1fnsyfV1VEnCKTb2dur.C', //  password:1234
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+        updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        id: 5,
+        email: 'Remover@gmail.com',
+        password: '$2b$10$Qiksk.UxZld647y0JtqjdeE33S8lQDnlpEo4GSV1CxXjOZCrpGeXK', //  password:1234
         createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
         updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
