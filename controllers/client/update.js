aa78bbc891e667518ab7fe940e76296480429ec6
Higor Anjos <higorc.anjos@gmail.com>
2022-07-17 17:38:50 -0300

    refactor: :recycle: mudando http semantic

--- controllers/client/update.js
+++ controllers/client/update.js
@@ -4,7 +4,7 @@ const update = async (req, res) => {
   const { client } = req;
   const clientUpdate = { id: client.id, ...req.body };
   await services.client.update(clientUpdate);
-  return res.status(200).json({ message: 'Cliente atualizado com sucesso' });
+  return res.status(201).json({ message: 'Cliente atualizado com sucesso' });
 };
 
 module.exports = update;
