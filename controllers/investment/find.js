dc345a3d6d5a7e71f3379288d92b69f30f62e323
Higor Anjos <higorc.anjos@gmail.com>
2022-07-19 22:48:10 -0300

    feat: :sparkles: endpoint para buscar um investimento

---
+++ controllers/investment/find.js
@@ -0,0 +1,9 @@
+const services = require('../../services');
+
+const find = async (req, res) => {
+  const { id } = req.params;
+  const investiment = await services.investiment.find(id);
+  return res.status(200).send(investiment);
+};
+
+module.exports = find;
