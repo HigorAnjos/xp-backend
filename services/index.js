f7d98eb6030b47932d45dfbcabeef6a72d9269c8
Higor Anjos <higorc.anjos@gmail.com>
2022-07-19 22:37:00 -0300

    feat: :sparkles: endpoits e importacoes de arquivos

--- services/index.js
+++ services/index.js
@@ -1,5 +1,6 @@
 const client = require('./client');
 const balance = require('./balance');
 const active = require('./active');
+const investiment = require('./investment');
 
-module.exports = { client, balance, active };
+module.exports = { client, balance, active, investiment };
