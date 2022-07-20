83657a93189055cbf96ff7872b2844139c32619d
Higor Anjos <higorc.anjos@gmail.com>
2022-07-19 22:49:06 -0300

    feat: :sparkles: endpoint para comprar um investimento

---
+++ controllers/investment/buy.js
@@ -0,0 +1,12 @@
+const services = require('../../services');
+
+const buy = async (req, res) => {
+  const { id: clientId } = req.client;
+  const { code, quantity } = req.body;
+
+  await services.investiment.buy(clientId, code, quantity);
+
+  return res.status(201).json({ message: 'Investimento efetuado com sucesso' });
+};
+
+module.exports = buy;
