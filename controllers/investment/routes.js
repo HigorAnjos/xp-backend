ccfc83833818ffbae9261417b0b20c6825978ab7
Higor Anjos <higorc.anjos@gmail.com>
2022-07-22 10:49:42 -0300

    feat(validations,investment): :sparkles: validacoes na rotas buy e sell

--- controllers/investment/routes.js
+++ controllers/investment/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const auth = require('../../middlewares/auth');
+const validations = require('../../middlewares/validations');
 const { find, list, buy, sell } = require('./index');
 
 const router = express.Router();
 
 router.get('/list', list);
-router.post('/buy', auth, buy);
-router.post('/sell', auth, sell);
+router.post('/buy', validations.investment.buy, auth, buy);
+router.post('/sell', validations.investment.sell, auth, sell);
 router.get('/:id', find);
 
 module.exports = router;
