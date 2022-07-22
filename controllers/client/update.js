b9566bda015f4c1bdce17650c16a320c0f743d22
Higor Anjos <higorc.anjos@gmail.com>
2022-07-21 21:39:51 -0300

    feat: :sparkles: cliente update retorna novo token caso sucesso

--- controllers/client/update.js
+++ controllers/client/update.js
@@ -3,8 +3,8 @@ const services = require('../../services');
 const update = async (req, res) => {
   const { client } = req;
   const clientUpdate = { id: client.id, ...req.body };
-  await services.client.update(clientUpdate);
-  return res.status(201).json({ message: 'Cliente atualizado com sucesso' });
+  const token = await services.client.update(clientUpdate);
+  return res.status(201).json({ token });
 };
 
 module.exports = update;
