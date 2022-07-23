70ede16e6f76ec2e6210c114799da7f4ddca742e
Higor Anjos <higorc.anjos@gmail.com>
2022-07-23 12:47:19 -0300

    refactor: :recycle:

--- controllers/investment/find.js
+++ controllers/investment/find.js
@@ -3,7 +3,7 @@ const services = require('../../services');
 const find = async (req, res) => {
   const { id } = req.params;
   const investiment = await services.investiment.find(id);
-  return res.status(200).send(investiment);
+  return res.status(200).json(investiment);
 };
 
 module.exports = find;
